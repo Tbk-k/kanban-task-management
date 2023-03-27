b3530431bbaac97419cea63f85ac0235b559b800
K <tabaka.karol@gmail.com>
2023-03-28 00:00:24 +0200

    ''

--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
-import AddNewTask from "../addNewTask/AddNewTask";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Modals from "../modals/Modals";
 import { StyledEmptyMessage, TaskListWrapper } from "./TaskList.styles";
-
+import { toggleAddNewTask } from "../../reducers/modalsSlice";
 const TaskList = () => {
-  const [addTask, setAddTask] = useState(false);
-  const handleAddTask = () => {
-    setAddTask((prev) => !prev);
+  const boards = useSelector((state) => state.taskBoard);
+  const activeBoard
+  const dispatch = useDispatch();
+
+  useEffect(()=>{},[])
+
+  let isEmptyBoard = true;
+  useEffect(() => {
+    isEmptyBoard = board.columns.length === 0;?
+  }, []);
+
+  const handleAddNewTask = () => {
+    dispatch(toggleAddNewTask());
   };
   return (
-    <TaskListWrapper>
-      <StyledEmptyMessage>
-        <h3>This board is empty. Create a new task to get started.</h3>
-        <button onClick={handleAddTask}>+ Add New Task</button>
-      </StyledEmptyMessage>
-      {addTask && <AddNewTask handleAddTask={handleAddTask} />}
-    </TaskListWrapper>
+    <>
+      <TaskListWrapper>
+        {isEmptyBoard && (
+          <StyledEmptyMessage>
+            <h3>This board is empty. Create a new task to get started.</h3>
+            <button onClick={handleAddNewTask}>+ Add New Task</button>
+          </StyledEmptyMessage>
+        )}
+      </TaskListWrapper>
+      <Modals />
+    </>
   );
 };
 
